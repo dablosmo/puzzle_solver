0c937e8efa34e406b0e7e9d5ae9d33bf5467ed9e
David Zhang <zhangdt@usc.edu>
2013-04-01 19:54:05 -0700

    Implemented puzzle move class with all necessary functions and methods. close #4

--- puzzle_move.cpp
+++ puzzle_move.cpp
@@ -1,7 +1,32 @@
 #include "puzzle_move.h"
+#include "puzzle_heur.h"
 
+using namespace std;
 
+PuzzleMove::PuzzleMove(Board &b) 
+{ 
+  ManhattanHeuristic calculator;
+  tileMove_ = -1;
+  b_ = new Board(b);
+  prev_ = NULL;
+  g_ = 0;
+  h_ = calculator.compute(b_->getTiles(), b_->getSize());
+}
 
+PuzzleMove::PuzzleMove(int tile, Board *b, PuzzleMove *parent)
+{
+  ManhattanHeuristic calculator;
+  tileMove_ = tile; 
+  b_ = b; 
+  prev_ = parent; 
+  g_ = prev_->g_+1; 
+  h_ = calculator.compute(b_->getTiles(), b_->getSize());
+}
+
+PuzzleMove::~PuzzleMove() 
+{
+  delete b_; 
+} 
 
 
 // Compare to PuzzleMoves based on f distance (needed for priority queue
@@ -29,5 +54,14 @@ bool PuzzleMove::operator>(const PuzzleMove& p) const
      return false;
   }
   
+}
+
+bool PuzzleMove::operator==(const PuzzleMove& p) const 
+{ 
+  if((g_+h_ == p.g_+p.h_) && (g_==p.g_)) 
+  {
+    return true; 
+  } 
+  return false;
 }
 
