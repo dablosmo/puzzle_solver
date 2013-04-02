0b298b86614f54c55abbe6824f7bd95b9c827162
David Zhang <zhangdt@usc.edu>
2013-04-01 23:08:27 -0700

    Added additional doxygen style comments to code, have been coding using doxygen style comments the whole way through. close #12

--- puzzle_heur.cpp
+++ puzzle_heur.cpp
@@ -23,15 +23,24 @@ int ManhattanHeuristic::calc(int *tiles, int size)
   int hDistance = 0;
   for (int i = 0; i < size; i++)
   { 
-    /// set solvedIndex equal to the value of the tile at index i, (position if puzzle is solved)
-    /// blank space is not technically a tile so don't have to calculate its solved index.
+    /// Set solvedIndex equal to the value of the tile at index i, (position if puzzle is solved)
+    /// Blank space is not technically a tile so don't have to calculate its solved index.
     if (tiles[i] != 0)
     {
       solvedIndex = tiles[i];
-      ///viewing the array of tiles as a 2d array allows us to calculate the shortest distance from the solved index easier
-      vDistance = abs( (solvedIndex % dim) - (i % dim) );   ///row numbers of the two indices
-      hDistance = abs( (solvedIndex/dim) - (i/dim) );  ///column numbers of the two indices
-      hscore = hscore + vDistance + hDistance;   ///addition of these two values is the distance the tile is away from the solved index.
+      /// Viewing the array of tiles as a 2d array allows us to calculate the shortest distance from the solved index easier
+      
+      vDistance = abs( (solvedIndex % dim) - (i % dim) );   
+      /// Take modulus of a tile's current index and its solved index by dim gives the  row numbers of the two indices
+      /// Subtracting the two indices and then getting the absolute value is the difference in rows
+      
+      hDistance = abs( (solvedIndex/dim) - (i/dim) );  
+      /// Taking the integer divisions of the two indives by dim gives the number of columns of each index
+      /// Subtracting the two indices and then getting the absolute value  returns the difference in columns
+      
+      hscore = hscore + vDistance + hDistance;   
+      /// Addition of these two differences is the distance the tile is away from the solved index.
+      /// This is done for every tile to get the total distance for the Manhattan Heuristic
     }
   }
   return hscore;
@@ -51,7 +60,7 @@ OutOfPlaceHeuristic::~OutOfPlaceHeuristic()
 int OutOfPlaceHeuristic::calc(int *tiles, int size)
 {
   int hscore = 0;
-  ///go through all the tiles and checks if each tile matches its index. If it doesnt, it adds one to the hscore until it is equal.
+  /// Go through all the tiles and checks if each tile matches its index. If it doesnt, it adds one to the hscore until it is equal.
   for (int i = 0; i < size; i++)
   {
     if ( (tiles[i] != 0) && (tiles[i] != i) )
